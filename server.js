94c3588baf477769c67244cf2fe917d1b9da4b01
Will Stern <willrstern@gmail.com>
2016-04-15 10:07:09 -0500

    changed server

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var express = require("express");
 
 express()
  .get("*", function(req, res) {
-    db("users").then(function(data) {
-      res.send(data);
-    });
+    res.send("ok");
  })
  .listen(3000, function(err) {
     if (err) {
